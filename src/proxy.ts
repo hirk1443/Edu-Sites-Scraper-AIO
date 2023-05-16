7fe89d9134808bd21147dcf5aa0af071f15bb31c
Long Nguyen <nguyen.long.908132@gmail.com>
2023-05-16 20:54:15 +0700

    Restart proxy + use random port

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,13 +3,10 @@ import ora from "ora";
 
 const cert = await mockttp.generateCACertificate();
 
-// const spinner = ora("Starting proxy server...").start();
+const spinner = ora("Starting proxy server...").start();
 const proxy = mockttp.getLocal({ https: cert });
 proxy.forUnmatchedRequest().thenPassThrough();
-/*
-await proxy.start(30000);
-
+await proxy.start();
 spinner.succeed("Started proxy server at " + proxy.url);
-*/
 
 export { proxy };
