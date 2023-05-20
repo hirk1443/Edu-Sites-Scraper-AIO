0f99d9b448948bc3505c92554e7dcf1e1762990f
Long Nguyen <nguyen.long.908132@gmail.com>
2023-05-20 20:19:53 +0700

    Ignore body size larger than 1MiB

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import ora from "ora";
 const cert = await mockttp.generateCACertificate();
 
 const spinner = ora("Starting proxy server...").start();
-const proxy = mockttp.getLocal({ https: cert });
+const proxy = mockttp.getLocal({ https: cert, maxBodySize: 1*1024*1024 });
 proxy.forUnmatchedRequest().thenPassThrough();
 await proxy.start();
 spinner.succeed("Started proxy server at " + proxy.url);
